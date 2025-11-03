e10f988014fbafa40e3d162cf66b8af0276ab9a2
Pratheep <pratheep.p.3369@gmail.com>
2025-11-04 01:52:22 +0530

    Day_02 completed

---
+++ Day_02.sql
@@ -0,0 +1,30 @@
+-- Practice Questions
+--1.
+SELECT name
+FROM patients
+WHERE age > 60;
+--2.
+SELECT staff_name
+FROM staff
+WHERE service = 'emergency';
+--     OR
+SELECT staff_name
+FROM staff
+WHERE service LIKE '%emergency';
+--3
+SELECT week
+FROM SERVICES_WEEKLY
+WHERE patients_request > 100;
+
+--Daily Challenge
+SELECT name
+FROM patients
+WHERE service = 'surgery' AND satisfaction < 70;
+
+
+
+
+
+
+
+
